2bcee9c8c607b83ed0af178e9c555e819f80f5c8
uuuuuu-ops <230643@astanait.edu.kz>
2025-06-26 10:48:09 +0500

    commit 5

--- node/src/main/java/org/example/service/impl/ConsumerServiceImpl.java
+++ node/src/main/java/org/example/service/impl/ConsumerServiceImpl.java
@@ -1,18 +1,16 @@
 package org.example.service.impl;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
-import org.example.service.ConsumerService;
-import org.example.service.MainService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.example.service.ConsumerService;
+import org.example.service.MainService;
 
 
-
+@Log4j
 @RequiredArgsConstructor
 @Service
-@Log4j
 public class ConsumerServiceImpl implements ConsumerService {
 
     private final MainService mainService;
@@ -20,29 +18,21 @@ public class ConsumerServiceImpl implements ConsumerService {
     @Override
     @RabbitListener(queues = "${spring.rabbitmq.queues.text-message-update}")
     public void consumetextservice(Update update) {
-        log.debug("TEXT : message is received");
+        log.debug("NODE: Text message is received");
         mainService.proccesTextMessage(update);
     }
 
     @Override
     @RabbitListener(queues = "${spring.rabbitmq.queues.doc-message-update}")
     public void consumedocservice(Update update) {
-        log.debug("DOC : message is received");
+        log.debug("NODE: Doc message is received");
         mainService.proccesDOCMessage(update);
-
     }
 
     @Override
     @RabbitListener(queues = "${spring.rabbitmq.queues.photo-message-update}")
     public void consumephotoservice(Update update) {
-
-        log.debug("Photo : message is received");
-
-        try {
-            mainService.proccesPhotoMessage(update);
-        } catch (Exception ex) {
-            log.error("Error while processing photo message", ex);
-        }
-
+        log.debug("NODE: Photo message is received");
+        mainService.proccesPhotoMessage(update);
     }
 }
