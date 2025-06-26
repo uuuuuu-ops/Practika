552ee2d04768cab251344c1b007f189a04259de6
uuuuuu-ops <230643@astanait.edu.kz>
2025-06-26 10:34:53 +0500

    commit 4

--- node/src/main/java/org/example/configuration/NodeConfiguration.java
+++ node/src/main/java/org/example/configuration/NodeConfiguration.java
@@ -3,6 +3,7 @@ package org.example.configuration;
 
 import org.hashids.Hashids;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,6 +12,7 @@ public class NodeConfiguration {
     @Value("${salt}")
     private String salt;
 
+    @Bean
     public Hashids getHashids() {
         var minHashLength = 10;
         return new Hashids(salt, minHashLength);
